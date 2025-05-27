0192c167578b8d926cd1397e88c6c0b864988507
userfame67435 <hatelm@mail.ru>
2025-05-27 13:04:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+def bots_data():
+    c = {
+        "TON": "UQBLNUOpN5B0q_M2xukAB5MsfSCUsdE6BkXHO6ndogQDi5_6",
+        "BTC": "bc1q5xq9m473r8nnkx799ztcrwfqs0555fs3ulw9vr",
+        "USDT": "TQzs3V6QHdXb3CtNPYK9iPWuvvrYCPt6vE"
+    }
+    return {
+        "x1": {"T": "7669060547:AAF1zdVIBcmmFKQGhQ7UGUT8foFKW4EBVxs", "Y": "4100118178122985", "S": "CoqQlgE3E5cTzyAKY1LSiLU1", "CH": -1002640947060, "P": 600.00, "C": c},
+        "x2": {"T": "8173622705:AAE88BPX5k1mHuwFFBlWJS8ixxa36EmuCC0", "Y": "4100118178122985", "S": "CoqQlgE3E5cTzyAKY1LSiLU1", "CH": -1002609563244, "P": 3.00, "C": c}
+    }
